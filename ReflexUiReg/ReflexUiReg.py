88eee88634e9620271711b577a5f1b6c57ff798e
jbcode <78531307+jbcodefr@users.noreply.github.com>
2024-09-21 16:53:01 +0200

    Emal Input and Button

--- ReflexUiReg/ReflexUiReg.py
+++ ReflexUiReg/ReflexUiReg.py
@@ -7,17 +7,40 @@ def render_auth_page():
     return rx.center(
                 rx.vstack(
                     rx.hstack(
-                        rx.color_mode.button(position="top-right"),
+                        rx.color_mode.button(),
                         justify="center",
                         width="100%"
                     ),
+                    rx.vstack(
+                        rx.heading("Creer un Compte",size="6",weight="regular"),
+                        rx.text(
+                            "Entrer votre email",
+                            font_size="10px",
+                            weight="medium",
+                            color_scheme="amber"
+                            ),
+                        width="100%",
+                        spacing="1",
+                        align="center"
+                    ),
+                    rx.input(
+                        width="100%",
+                        placeholder="myemail@domaine.com"  
+                    ),
+                    rx.button(
+                        "Se Connecter avec votre Email",
+                        width="100%",
+                        cursor="pointer",
+                        variant="surface",
+                        color_scheme="bronze"
+                    ),
                     width="100%",
-                    max_width="10%",
+                    max_width="50%",
                     height="100%",
                     justify="center",
                     align="center"
                 ),
-        width="100",
+        width="100%",
         height="100vh"
     )
 
