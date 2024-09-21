eb0c603e54bd238036a2dfbf47999c3368f57412
jbcode <78531307+jbcodefr@users.noreply.github.com>
2024-09-21 16:33:56 +0200

    Adding Button Dark mode (color_mode)

--- ReflexUiReg/ReflexUiReg.py
+++ ReflexUiReg/ReflexUiReg.py
@@ -5,8 +5,27 @@ import reflex as rx
 @rx.page("/","Auth")
 def render_auth_page():
     return rx.center(
+                rx.vstack(
+                    rx.hstack(
+                        rx.color_mode.button(position="top-right"),
+                        justify="center",
+                        width="100%"
+                    ),
+                    width="100%",
+                    max_width="10%",
+                    height="100%",
+                    justify="center",
+                    align="center"
+                ),
         width="100",
         height="100vh"
     )
 
-app = rx.App(theme=rx.theme( appearance="light", accent_color="crimson"))
+app = rx.App(
+        theme=rx.theme(
+        appearance="light",
+        has_background=True,
+        radius="large",
+        accent_color="teal",
+    )
+)
